e703093201ad0a82fb9cbe45af018d53435351c9
Edgar Damatian <damatian.e@gmail.com>
2024-01-18 14:32:11 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const mainNode = document.querySelector('main#main');
+if (mainNode) {
+  mainNode.remove();
+}
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "EDGAR DAMATIAN is the champion";
